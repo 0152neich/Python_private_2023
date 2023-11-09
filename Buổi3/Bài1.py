6ef07e3d4dc06d9aa7dcf82602aea504c55eadba
0152neich <duychien25102004@gmail.com>
2023-11-09 19:49:59 +0700

    fix:ex1

--- Buổi3/Bài1.py
+++ Buổi3/Bài1.py
@@ -1,7 +1,11 @@
+import math
 a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
 e = int(input())
 f = int(input())
-print ('KHÔNG PHẢI TAM GIÁC') if b - a == d - c == f - e else print('TAM GIÁC')
+m = math.sqrt((c - a)**2 + (d - b)**2)
+n = math.sqrt((e - c)**2 + (f - d)**2)
+k = math.sqrt((a - e)**2 + (b - f)**2)
+print('TAM GIÁC') if (m + n > k and n + k > m and k + m > n) else print('KHÔNG PHẢI TAM GIÁC')
--- Buổi3/Bài3.py
+++ Buổi3/Bài3.py
@@ -1,15 +1,15 @@
 i = int(input())
-s = input()
-a = int(input())
-b = int(input())
-x = ''
-if a + b >= 190:
-    x = 'Xuất sắc'
-elif a + b >= 150:
-    x = 'Giỏi'
-elif a + b >= 100:
-    x = 'Khá'
-else:
-    x = 'Kém'
-for j in range(i): 
+for j in range(i):
+    s = input()
+    a = int(input())
+    b = int(input())
+    x = ''
+    if a + b >= 190:
+        x = 'Xuất sắc'
+    elif a + b >= 150:
+        x = 'Giỏi'
+    elif a + b >= 100:
+        x = 'Khá'
+    else:
+        x = 'Kém'
     print(j + 1, s, a + b, x)

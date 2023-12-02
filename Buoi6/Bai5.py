e46cc11d34fabfec9ddb307ef2129dac52a09633
0152neich <duychien25102004@gmail.com>
2023-12-02 12:57:10 +0700

    update: Bai5

--- Buoi6/Bai5.py
+++ Buoi6/Bai5.py
@@ -1,29 +1,22 @@
-def find_gcd(a, b):
+def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
-def simplify_fraction(ts, ms):
-    gcd = find_gcd(ts, ms)
-    simple_ts = ts // gcd
-    simple_ms = ms // gcd
-    return simple_ts, simple_ms
+class PS():
+    def __init__(self, tuSo, mauSo) -> None:
+        self.tuSo = tuSo
+        self.mauSo = mauSo
+        
+    def __mul__(self, other):
+        tuSo = self.tuSo * other.tuSo
+        mauSo = self.mauSo * other.mauSo
+        gcd_ = gcd(tuSo, mauSo)
+        return PS(tuSo // gcd_, mauSo // gcd_)
 
-def product_of_fractions(n):
-    total_ts = 1
-    total_ms = 1
+s = PS(1, 1) 
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    s *= PS(a, b)
+print(s.tuSo, s.mauSo)
 
-    for _ in range(n):
-        a, b = map(int, input().split())
-        total_ts *= a
-        total_ms *= b
-
-    gcd = find_gcd(total_ts, total_ms)
-    simple_ts = total_ts // gcd
-    simple_ms = total_ms // gcd
-
-    return simple_ts, simple_ms
-
-n = int(input())
-result_ts, result_ms = product_of_fractions(n)
-print(result_ts, result_ms)
